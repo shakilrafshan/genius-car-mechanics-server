7c0eaf6c9f6b719d307c800cf65cdbd9472c4d26
shakilrafshan <shakilrafshan@gmail.con>
2021-10-28 20:46:47 +0600

    shakil

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors=require('cors');
 require('dotenv').config();
 const app=express();
 const port=process.env.PORT || 5000;
-//middlewar
+//middleware
 app.use(cors());
 app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ep343.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
